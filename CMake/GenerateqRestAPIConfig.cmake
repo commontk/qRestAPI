c2f9df1cb90d15cc7ed14477df7df2a253db710b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-05-21 00:10:58 -0400

    Convert CMake-language commands to lower case
    Ancient CMake versions required upper-case commands.  Later command
    names became case-insensitive.  Now the preferred style is lower-case.
    
    Run the following shell code:
    
    cmake --help-command-list |
    grep -v "cmake version" |
    while read c; do
        echo 's/\b'"$(echo $c | tr '[:lower:]' '[:upper:]')"'\(\s*\)(/'"$c"'\1(/g'
    done >convert.sed &&
    git ls-files -z -- bootstrap '*.cmake' '*.cmake.in' '*CMakeLists.txt' |
    egrep -z -v '^(Utilities/cm|Source/kwsys/)' |
    xargs -0 sed -i -f convert.sed &&
    rm convert.sed

--- CMake/GenerateqRestAPIConfig.cmake
+++ CMake/GenerateqRestAPIConfig.cmake
@@ -1,17 +1,17 @@
 
-CONFIGURE_FILE(
+configure_file(
   ${qRestAPI_SOURCE_DIR}/CMake/UseqRestAPI.cmake.in
   ${qRestAPI_BINARY_DIR}/UseqRestAPI.cmake COPYONLY)
 
 # Include directories
-SET(qRestAPI_INCLUDE_DIRS_CONFIG ${qRestAPI_SOURCE_DIR})
+set(qRestAPI_INCLUDE_DIRS_CONFIG ${qRestAPI_SOURCE_DIR})
 
 # UseqRestAPI file
-SET(qRestAPI_USE_FILE_CONFIG ${qRestAPI_BINARY_DIR}/UseqRestAPI.cmake)
+set(qRestAPI_USE_FILE_CONFIG ${qRestAPI_BINARY_DIR}/UseqRestAPI.cmake)
 
-EXPORT(TARGETS qRestAPI FILE ${qRestAPI_BINARY_DIR}/qRestAPIExports.cmake)
+export(TARGETS qRestAPI FILE ${qRestAPI_BINARY_DIR}/qRestAPIExports.cmake)
 
 # Configure qRestAPIConfig.cmake
-CONFIGURE_FILE(
+configure_file(
   ${qRestAPI_SOURCE_DIR}/CMake/qRestAPIConfig.cmake.in
   ${qRestAPI_BINARY_DIR}/qRestAPIConfig.cmake @ONLY)
